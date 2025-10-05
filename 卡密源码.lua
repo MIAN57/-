65a4530b660fff5041505b04ac1accac52b5ac61
anne <1357377308@qq.com>
2025-10-05 12:50:45 +0800

    Update 卡密源码.lua

--- 卡密源码.lua
+++ 卡密源码.lua
@@ -1,6 +1,6 @@
-local Library = loadstring(game:HttpGet("不告诉你🤑"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/nfpw/Simple-KeySystem-Roblox/refs/heads/main/Library.lua"))()
 
-local KeyWindow = Library:CreateWindow("黄脚本卡密系统", UDim2.fromOffset(300, 180))
+local KeyWindow = Library:CreateWindow("汉化中心卡密系统", UDim2.fromOffset(300, 180))
 
 local KeyBox = KeyWindow:AddTextBox("请输入卡密...", UDim2.new(0.1, 0, 0.15, 0))
 local RememberToggle = KeyWindow:AddToggle("记住卡密", UDim2.new(0.1, 0, 0.36, 0), false)
@@ -15,20 +15,20 @@ if SavedKey then
 end
 
 local SubmitButton = KeyWindow:AddButton("验证卡密", UDim2.new(0.1, 0, 0.65, 0), function()
-    if KeyBox.Text == "hyjnb" then
+    if KeyBox.Text == "SHJKHYJ" then
         if RememberToggle.GetState() then
             Library:SaveKey(KeyPath, FileName, KeyBox.Text)
         end
         KeyWindow:Close()
         local success, err = pcall(function()
-            loadstring(game:HttpGet("不告诉你🤑"))()
+            loadstring(game:HttpGet(""))()
         end)
         if not success then
             warn("加载远程脚本时出错: ", err)
         end
     else
         local player = cloneref(game:GetService("Players")).LocalPlayer
-        player:Kick("卡密错误请加入腾讯频道：pd43735421")
+        player:Kick("卡密错误请加入QQ群：885775764")
         if player then
             player:Destroy()
         end
@@ -37,7 +37,7 @@ end)
 
 local GetKeyButton = KeyWindow:AddButton("获取卡密", UDim2.new(0.55, 0, 0.65, 0), function()
     if setclipboard then
-        setclipboard("请加入腾讯频道：pd43735421")
+        setclipboard("请加入QQ群：885775764")
     else
         warn("setclipboard is not available.")
     end
